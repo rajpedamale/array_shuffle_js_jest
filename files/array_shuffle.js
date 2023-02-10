592ef370b01f6ef0500c3120872311aadc01253e
9C94bR <9C94bR@cyber-dojo.org>
2023-02-10 21:40:41 +0000

    16 ran tests, predicted green, got red

--- files/array_shuffle.js
+++ files/array_shuffle.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const permutate = (inputArray, outputArray) => {
-  return outputArray
+  inputArray.forEach((item, index) => {
+    const shuffleIndex = Math.floor(Math.random() * inputArray.length);
+    const shuffleItem = outputArray[shuffleIndex];
+    outputArray[shuffleIndex] = item;
+    outputArray[index] = shuffleItem;
+  });
+  return outputArray;
 };
 
 function shuffle(inputArray) {
