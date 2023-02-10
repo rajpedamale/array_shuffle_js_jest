b0366c0bfc684e6275084ed99343e3e2eefd0c42
9C94bR <9C94bR@cyber-dojo.org>
2023-02-10 21:15:31 +0000

    4 ran tests, predicted red, got red

--- files/array_shuffle.test.js
+++ files/array_shuffle.test.js
@@ -2,10 +2,24 @@
 
 const shuffle = require('./array_shuffle');
 
-const sampleArray = [1, 2, 3, 4, 5, 6]
+const sampleArray = [1, 2, 3, 4, 5, 6];
+
+const checkPermutation = (source, target) => {
+  let isPermutation = source.length === target.length;
+  if (isPermuation) {
+    isPermuation = source.reduce((acc, value) => {
+      return acc && target.includes(value);
+    }, isPermutation);
+  }
+  return isPermutation;
+};
 
 describe('array_shuffle', () => {
   it('should return an array', () => {
     expect(Array.isArray(shuffle(sampleArray))).toBeTruthy();
+  });
+
+  it('should return a permutation of input Array', () => {
+    expect(checkPermutation(sampleArray, shuffle(sampleArray))).toBeTruthy();
   });
 });
